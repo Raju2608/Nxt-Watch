a2081b8a6670d653f92d1d6ffa4638eaefb0ee16
Raju2608 <151773258+Raju2608@users.noreply.github.com>
2024-04-05 15:44:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
 
   saveVideoButtonClicked = data => {
     const {savedVideosList} = this.state
-    this.setState({savedVideosList: [...savedVideosList, data]})
+    this.setState({savedVideosList: [...savedVideosList]})
   }
 
   render() {
